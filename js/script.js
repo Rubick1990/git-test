97d7feb362a5d132156eb3dd1c12320c2135c52d
Rubick1990 <anfitrion1990@gmail.com>
2023-04-07 20:09:55 -0500

    Segunda alerta

---
+++ js/script.js
@@ -0,0 +1,14 @@
+cont = 0;
+function saludo(){
+    if(cont>0){
+        no();
+    }
+    else{
+    alert("Hola alumnos");
+    }
+    cont++;
+}
+
+function no(){
+    alert("No lo presiones denuevo");
+}
